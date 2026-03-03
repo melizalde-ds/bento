1f2b9a071cb46f5b265b16c2282cc0378edc8826
Miguel Elizalde <miguel.elizalde.19.98@live.com>
2026-03-03 13:09:02 -0700

    Refactor Manifest struct and enhance package handling with improved data structures

--- src/manifest.rs
+++ src/manifest.rs
@@ -1,10 +1,37 @@
+use std::{collections::BTreeMap, path::Path};
+
+use anyhow::{Result, bail};
+use serde::{Deserialize, Serialize};
+
 const MANIFEST_FILE: &str = "bento.toml";
 
-struct Manifest {
+#[derive(Deserialize, Serialize)]
+pub struct Manifest {
     project: ProjectMetadata,
     packages: PackagesTable,
 }
 
+impl Manifest {
+    pub fn load() -> Result<Self> {
+        if !Path::new(MANIFEST_FILE).exists() {
+            bail!(
+                "Project not initialized in this directory. Please run `bento init` to create a new project."
+            );
+        }
+
+        let content = std::fs::read_to_string(MANIFEST_FILE)?;
+        let config: Self = toml::from_str(&content)?;
+        Ok(config)
+    }
+
+    pub fn save(&self) -> Result<()> {
+        let content = toml::to_string(self)?;
+        std::fs::write(MANIFEST_FILE, content)?;
+        Ok(())
+    }
+}
+
+#[derive(Deserialize, Serialize)]
 struct ProjectMetadata {
     name: String,
     version: String,
@@ -12,6 +39,22 @@ struct ProjectMetadata {
     author: String,
 }
 
+#[derive(Deserialize, Serialize, PartialOrd, Ord, PartialEq, Eq)]
 struct PackagesTable {
-    packages: Option<String>,
+    packages: Option<BTreeMap<PackageKey, PackageSpec>>,
+}
+
+#[derive(Deserialize, Serialize, PartialOrd, Ord, PartialEq, Eq, Hash)]
+struct PackageKey(String);
+
+#[derive(Deserialize, Serialize, PartialOrd, Ord, PartialEq, Eq, Hash)]
+enum PackageSpec {
+    Version(String),
+    Features(PackageFeatures),
+}
+
+#[derive(Deserialize, Serialize, PartialOrd, Ord, PartialEq, Eq, Hash)]
+struct PackageFeatures {
+    version: String,
+    features: Vec<String>,
 }
